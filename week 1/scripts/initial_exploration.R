193b36d25dd3f153af599539ebcc00ee4fcf881d
Ella Simms <gxw24bhu@uea.ac.uk>
2026-02-05 18:10:33 +0000

    complete chapter 4

--- week 1/scripts/initial_exploration.R
+++ week 1/scripts/initial_exploration.R
@@ -159,3 +159,27 @@ penguins_clean_names |>
     sep = "(?=\\()" # regex pattern: split before the '('
   ) |> 
   mutate(full_latin_name = str_remove_all(full_latin_name, "[\\(\\)]"))
+
+#tutorial 2
+penguins_demo <- penguins_clean_names |> 
+  slice(1:50) |> 
+  bind_rows(slice(penguins_clean_names, c(1,5,10,15,30)))
+#adding duplicates
+
+# check for whole duplicate 
+# rows in the data
+penguins_clean_names |> 
+  filter(duplicated(across(everything())))
+sum() 
+#still 0
+# Keep only unduplicated data with !
+penguins_demo |> 
+  filter(!duplicated(across(everything())))
+
+#n_distinct counts the number of distinct variables
+penguins_clean_names |> 
+  summarise(
+    n = n(),
+    n_distinct(individual_id)
+  )
+#344 variables, 190 distinct
